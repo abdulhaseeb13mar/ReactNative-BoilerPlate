ac516151b26f7e4cf2d58a5af763fc5c0370174e
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-01-24 01:09:36 +0500

    react native boilerplate

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {RootSiblingParent} from 'react-native-root-siblings';
+import {Provider} from 'react-redux';
+import store from './src/store/store';
+import Routes from './routes';
+
+const App: () => React$Node = () => {
+  return (
+    <RootSiblingParent>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </RootSiblingParent>
+  );
+};
+
+export default App;
