96b44115629a72aa73924414d73b73f0ee865d12
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-01-29 03:09:54 +0500

    colors added

--- src/shared/Theme.js
+++ src/shared/Theme.js
@@ -1,6 +1,12 @@
 import {Dimensions, Platform, PixelRatio} from 'react-native';
 
-export const colors = {};
+export const colors = {
+  primary: '#0a9aea',
+  secondary: '#eff7fd',
+  lightBackground: '#eeeff0',
+  lightGrey: 'rgb(245,245,245)',
+  darkGray: '#666565',
+};
 
 export const metrics = {
   width: Dimensions.get('window').width,
